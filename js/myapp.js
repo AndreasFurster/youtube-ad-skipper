959328aede1aa6196cc6f483377d56a7e1c5e764
Andreas Furster <andreas.furster@hotmail.com>
2019-04-25 20:47:23 +0200

    Improved readability and skipping in general

--- js/myapp.js
+++ js/myapp.js
@@ -1,59 +1,55 @@
 !(function() {
   "use strict";
-  
-  var t = "on",
-    e = e || [];
-  function n() {
-    var t = document.getElementsByClassName("video-stream")[0] || !1;
-    t &&
-      (o(),
-      (t.onprogress = t.ontimeupdate = function() {
-        o();
-      }));
+
+  var enabled = "on";
+
+  var searchClasses = [
+    ".videoAdUiSkipButton.videoAdUiAction.videoAdUiFixedPaddingSkipButton",
+    ".ytp-ad-skip-button",
+    ".ytp-ad-overlay-close-button"
+  ];
+
+  function attachSkipper() {
+    var streamElement =
+      document.getElementsByClassName("video-stream")[0] || false;
+
+    if (streamElement) {
+      skipperdySkip();
+      streamElement.onprogress = streamElement.ontimeupdate = skipperdySkip;
+    }
   }
-  function o() {
-    var t = [
-      ".videoAdUiSkipButton.videoAdUiAction.videoAdUiFixedPaddingSkipButton",
-      ".ytp-ad-skip-button"
-    ];
-    for (var e in t) i(t[e]);
-  }
-  function i(t) {
-    !1 !== r(t) &&
+
+  function skipperdySkip() {
+    searchClasses.forEach(searchClass => {
       setTimeout(function() {
-        var n;
-        !1 !== r(t) &&
-          (document.querySelector(t).click(),
-          (function() {
-            var t = ".ytp-ad-button-text";
-            if (0 == document.querySelectorAll(t).length) return;
-            var n = document.querySelector(t).innerText;
-          })(),
-          (n = t));
-      }, 100);
+        if (enabled == "on") {
+          var element = document.querySelector(searchClass);
+          if (element != null) {
+            element.click();
+          }
+        }
+      }, 50);
+    });
   }
-  function r(e) {
-    if ("off" === t) return !1;
-    if (0 == document.querySelectorAll(e).length) return !1;
-    var n = ".ytp-ad-skip-button-slot";
-    return (
-      0 != document.querySelectorAll(n).length &&
-      "none" !== window.getComputedStyle(document.querySelector(n)).display
-    );
-  }
-  !(function() {
-    n(),
-    document.addEventListener("spfdone", n),
-    document.addEventListener("transitionend", n),
-    document.addEventListener("DOMContentLoaded", n),
-    window.addEventListener("popstate", n),
-    "onhashchange" in window && (window.onhashchange = n);
-    (window.document.onload = n), (window.onload = n), setTimeout(n, 1e3);
-  })(),
-    chrome.storage.sync.get(["youtube_ad_skip_trigger"], function(e) {
-      t = "off" == e.youtube_ad_skip_trigger ? "off" : "on";
-    }),
+
+  // Attach skipper loads of times. Don't give them any chance to disable it.
+  attachSkipper();
+  document.addEventListener("spfdone", attachSkipper);
+  document.addEventListener("transitionend", attachSkipper);
+  document.addEventListener("DOMContentLoaded", attachSkipper);
+  window.addEventListener("popstate", attachSkipper);
+
+  if ("onhashchange" in window) window.onhashchange = attachSkipper;
+
+  window.document.onload = attachSkipper;
+  window.onload = attachSkipper;
+
+  setTimeout(attachSkipper, 2000);
+
+  chrome.storage.sync.get(["youtube_ad_skip_trigger"], function(e) {
+    enabled = "off" == e.youtube_ad_skip_trigger ? "off" : "on";
+  }),
     chrome.storage.onChanged.addListener(function(e, n) {
-      t = "off" == e.youtube_ad_skip_trigger.newValue ? "off" : "on";
+      enabled = "off" == e.youtube_ad_skip_trigger.newValue ? "off" : "on";
     });
 })();
