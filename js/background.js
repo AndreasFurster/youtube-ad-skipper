1014f2d5f62cc25ff07a650ceefc4847f823304c
Andreas Furster <andreas.furster@hotmail.com>
2019-04-25 20:06:46 +0200

    Removed tracking

--- js/background.js
+++ js/background.js
@@ -1,1 +1,30 @@
-var currentPermission="on",bg_color={on:"#009933",off:"#ff3300"};chrome.storage.sync.set({youtube_ad_skip_trigger:currentPermission},function(){chrome.browserAction.setBadgeText({text:currentPermission}),chrome.browserAction.setBadgeBackgroundColor({color:bg_color[currentPermission]})}),chrome.storage.sync.get(["youtube_ad_skip_trigger"],function(r){currentPermission="off"===r.youtube_ad_skip_trigger?"off":"on",chrome.browserAction.setBadgeText({text:currentPermission}),chrome.browserAction.setBadgeBackgroundColor({color:bg_color[currentPermission]})}),chrome.browserAction.onClicked.addListener(function(){currentPermission="off"===currentPermission?"on":"off",chrome.storage.sync.set({youtube_ad_skip_trigger:currentPermission},function(){chrome.browserAction.setBadgeText({text:currentPermission}),chrome.browserAction.setBadgeBackgroundColor({color:bg_color[currentPermission]})})});
+var currentPermission = "on",
+  bg_color = { on: "#009933", off: "#ff3300" };
+chrome.storage.sync.set(
+  { youtube_ad_skip_trigger: currentPermission },
+  function() {
+    chrome.browserAction.setBadgeText({ text: currentPermission }),
+      chrome.browserAction.setBadgeBackgroundColor({
+        color: bg_color[currentPermission]
+      });
+  }
+),
+  chrome.storage.sync.get(["youtube_ad_skip_trigger"], function(r) {
+    (currentPermission = "off" === r.youtube_ad_skip_trigger ? "off" : "on"),
+      chrome.browserAction.setBadgeText({ text: currentPermission }),
+      chrome.browserAction.setBadgeBackgroundColor({
+        color: bg_color[currentPermission]
+      });
+  }),
+  chrome.browserAction.onClicked.addListener(function() {
+    (currentPermission = "off" === currentPermission ? "on" : "off"),
+      chrome.storage.sync.set(
+        { youtube_ad_skip_trigger: currentPermission },
+        function() {
+          chrome.browserAction.setBadgeText({ text: currentPermission }),
+            chrome.browserAction.setBadgeBackgroundColor({
+              color: bg_color[currentPermission]
+            });
+        }
+      );
+  });
--- js/myapp.js
+++ js/myapp.js
@@ -1,1 +1,59 @@
-!function(){"use strict";console.log("YSAT");var t="on",e=e||[];function n(){var t=document.getElementsByClassName("video-stream")[0]||!1;t&&(o(),t.onprogress=t.ontimeupdate=function(){o()})}function o(){var t=[".videoAdUiSkipButton.videoAdUiAction.videoAdUiFixedPaddingSkipButton",".ytp-ad-skip-button"];for(var e in t)i(t[e])}function i(t){!1!==r(t)&&setTimeout(function(){var n;!1!==r(t)&&(document.querySelector(t).click(),function(){var t=".ytp-ad-button-text";if(0==document.querySelectorAll(t).length)return;var n=document.querySelector(t).innerText;e.push(["_trackEvent","ad_btn_text",n])}(),n=t,e.push(["_trackEvent",n,"clicked"]))},100)}function r(e){if("off"===t)return!1;if(0==document.querySelectorAll(e).length)return!1;var n=".ytp-ad-skip-button-slot";return 0!=document.querySelectorAll(n).length&&"none"!==window.getComputedStyle(document.querySelector(n)).display}!function(){function t(){n()}n(),document.addEventListener("spfdone",function(){n()}),document.addEventListener("transitionend",function(t){n()}),document.addEventListener("DOMContentLoaded",function(){n()}),window.addEventListener("popstate",function(){n()}),"onhashchange"in window&&(window.onhashchange=t);window.document.onload=t,window.onload=t,setTimeout(function(){n()},1e3)}(),e.push(["_setAccount","UA-57562361-4"]),e.push(["_trackPageview"]),function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://ssl.google-analytics.com/ga.js";var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}(),chrome.storage.sync.get(["youtube_ad_skip_trigger"],function(e){t="off"==e.youtube_ad_skip_trigger?"off":"on"}),chrome.storage.onChanged.addListener(function(e,n){t="off"==e.youtube_ad_skip_trigger.newValue?"off":"on"})}();
+!(function() {
+  "use strict";
+  
+  var t = "on",
+    e = e || [];
+  function n() {
+    var t = document.getElementsByClassName("video-stream")[0] || !1;
+    t &&
+      (o(),
+      (t.onprogress = t.ontimeupdate = function() {
+        o();
+      }));
+  }
+  function o() {
+    var t = [
+      ".videoAdUiSkipButton.videoAdUiAction.videoAdUiFixedPaddingSkipButton",
+      ".ytp-ad-skip-button"
+    ];
+    for (var e in t) i(t[e]);
+  }
+  function i(t) {
+    !1 !== r(t) &&
+      setTimeout(function() {
+        var n;
+        !1 !== r(t) &&
+          (document.querySelector(t).click(),
+          (function() {
+            var t = ".ytp-ad-button-text";
+            if (0 == document.querySelectorAll(t).length) return;
+            var n = document.querySelector(t).innerText;
+          })(),
+          (n = t));
+      }, 100);
+  }
+  function r(e) {
+    if ("off" === t) return !1;
+    if (0 == document.querySelectorAll(e).length) return !1;
+    var n = ".ytp-ad-skip-button-slot";
+    return (
+      0 != document.querySelectorAll(n).length &&
+      "none" !== window.getComputedStyle(document.querySelector(n)).display
+    );
+  }
+  !(function() {
+    n(),
+    document.addEventListener("spfdone", n),
+    document.addEventListener("transitionend", n),
+    document.addEventListener("DOMContentLoaded", n),
+    window.addEventListener("popstate", n),
+    "onhashchange" in window && (window.onhashchange = n);
+    (window.document.onload = n), (window.onload = n), setTimeout(n, 1e3);
+  })(),
+    chrome.storage.sync.get(["youtube_ad_skip_trigger"], function(e) {
+      t = "off" == e.youtube_ad_skip_trigger ? "off" : "on";
+    }),
+    chrome.storage.onChanged.addListener(function(e, n) {
+      t = "off" == e.youtube_ad_skip_trigger.newValue ? "off" : "on";
+    });
+})();
